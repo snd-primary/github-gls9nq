a4931501b48280b29506fdad83d0f2689756f142
snd-webdev <99936282+snd-webdev@users.noreply.github.com>
2023-08-17 01:58:07 +0900

    1 file modified, 3 added, 1 deleted

--- src/layouts/Layout.css.ts
+++ src/layouts/Layout.css.ts
@@ -1,6 +1,12 @@
-import { style } from "@vanilla-extract/css";
+import { style } from '@vanilla-extract/css';
 
 export const layout = style({
   background: '#000',
   color: '#fff',
-})
+  width: '100%',
+  height: '100%',
+  margin: 0,
+  padding: 0,
+  boxSizing: 'border-box',
+  display: 'grid',
+});
